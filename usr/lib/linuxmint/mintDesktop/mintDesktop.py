3ee9c5e800addfd206ad426593581637a6a2f362
Ikey Doherty <contactjfreak@googlemail.com>
2010-03-29 19:40:44 +0100

    Added proper gconf binding, reduced code replication

--- usr/lib/linuxmint/mintDesktop/mintDesktop.py
+++ usr/lib/linuxmint/mintDesktop/mintDesktop.py
@@ -53,22 +53,14 @@ class MintDesktop:
 	def __init__(self):
 		
 		self.gladefile = '/usr/lib/linuxmint/mintDesktop/mintDesktop.glade'
+
 		self.wTree = gtk.glade.XML(self.gladefile, "main_window") 
 
 		self.wTree.get_widget("main_window").connect("destroy", gtk.main_quit)
 		self.wTree.get_widget("button_cancel").connect("clicked", gtk.main_quit)
-		self.wTree.get_widget("checkbox_computer").connect("clicked", lambda x: self.set_bool("/apps/nautilus/desktop/computer_icon_visible", x))
-		self.wTree.get_widget("checkbox_home").connect("clicked", lambda x: self.set_bool("/apps/nautilus/desktop/home_icon_visible", x))
-		self.wTree.get_widget("checkbox_network").connect("clicked", lambda x: self.set_bool("/apps/nautilus/desktop/network_icon_visible", x))
-		self.wTree.get_widget("checkbox_trash").connect("clicked", lambda x: self.set_bool("/apps/nautilus/desktop/trash_icon_visible", x))
-		self.wTree.get_widget("checkbox_volumes").connect("clicked", lambda x: self.set_bool("/apps/nautilus/desktop/volumes_visible", x))
-		self.wTree.get_widget("checkbox_compositing").connect("clicked", lambda x: self.set_bool("/apps/metacity/general/compositing_manager", x))
-
 		self.wTree.get_widget("combo_wmlayout").append_text(_("Traditional style"))
 		self.wTree.get_widget("combo_wmlayout").append_text(_("Mac style"))
 		self.wTree.get_widget("combo_wmlayout").append_text(_("Ubuntu style"))
-
-		
 
 		# i18n
 		self.wTree.get_widget("main_window").set_title(_("Desktop Settings"))
@@ -82,30 +74,18 @@ class MintDesktop:
 		self.wTree.get_widget("checkbox_volumes").set_label(_("Mounted Volumes"))
 		self.wTree.get_widget("checkbox_compositing").set_label(_("Gnome compositing"))
 
-		# Get the current configuration
-		confComputer = self.get_bool("/apps/nautilus/desktop/computer_icon_visible")
-		if (confComputer):
-			self.wTree.get_widget("checkbox_computer").set_active(1)
-		confHome = self.get_bool("/apps/nautilus/desktop/home_icon_visible")
-		
-		if (confHome):
-			self.wTree.get_widget("checkbox_home").set_active(1)
-		confNetwork = self.get_bool("/apps/nautilus/desktop/network_icon_visible")
-		
-		if (confNetwork):
-			self.wTree.get_widget("checkbox_network").set_active(1)
-		confTrash = self.get_bool("/apps/nautilus/desktop/trash_icon_visible")
-		
-		if (confTrash):
-			self.wTree.get_widget("checkbox_trash").set_active(1)
-		confVolumes = self.get_bool("/apps/nautilus/desktop/volumes_visible")
-		
-		if (confVolumes):
-			self.wTree.get_widget("checkbox_volumes").set_active(1)
-		confCompositing = self.get_bool("/apps/metacity/general/compositing_manager")
-		
-		if (confCompositing):
-			self.wTree.get_widget("checkbox_compositing").set_active(1)
+		# tell gconf we want to be notified when these change
+		client = gconf.client_get_default()
+		client.add_dir("/apps/nautilus/desktop", gconf.CLIENT_PRELOAD_NONE)
+		client.add_dir("/apps/metacity/general", gconf.CLIENT_PRELOAD_NONE)
+
+		# initialise the checkboxes		
+		self.init_checkbox("/apps/nautilus/desktop/computer_icon_visible", "checkbox_computer")
+		self.init_checkbox("/apps/nautilus/desktop/home_icon_visible", "checkbox_home")
+		self.init_checkbox("/apps/nautilus/desktop/network_icon_visible", "checkbox_network")
+		self.init_checkbox("/apps/nautilus/desktop/trash_icon_visible", "checkbox_trash")
+		self.init_checkbox("/apps/nautilus/desktop/volumes_visible", "checkbox_volumes")
+		self.init_checkbox("/apps/metacity/general/compositing_manager", "checkbox_compositing")
 
 		# slightly more complicated. find the window manager button layout in use..
 		confLayout = self.get_string("/apps/metacity/general/button_layout")
@@ -120,7 +100,41 @@ class MintDesktop:
 			# TODO: Add checking for custom layouts here, store the layout etc.
 
 		self.wTree.get_widget("combo_wmlayout").connect("changed", self.applyWMChanges)
-		
+
+
+	''' Initialise the CheckButton with a gconf value, then bind it with the gconf system '''
+	def init_checkbox(self, key, name):
+		widget = self.wTree.get_widget(name)
+		conf = self.get_bool(key)
+		widget.set_active(conf)
+		widget.connect("clicked", lambda x: self.set_bool(key, x))
+		self.add_notify(key, widget)
+
+	''' adds a notify system... '''
+	def add_notify(self, key, widget):
+		client = gconf.client_get_default()
+		notify_id = client.notify_add(key, self.key_changed_callback, widget)
+		widget.set_data('notify_id', notify_id)
+		widget.set_data('client', client)
+		widget.connect("destroy", self.destroy_callback)
+
+
+	''' destroy the associated notifications '''
+	def destroy_callback (self, widget):
+		client = widget.get_data ('client')
+    		notify_id = widget.get_data ('notify_id')
+
+		if notify_id:
+			client.notify_remove (notify_id)
+
+	''' Callback for gconf. update our internal values '''
+	def key_changed_callback (self, client, cnxn_id, entry, widget):
+		# deal with all boolean (checkboxes)
+		if (type(widget) == gtk.CheckButton):
+			if(entry.value.type == gconf.VALUE_BOOL):
+				value = entry.value.get_bool()
+				if(widget):
+					widget.set_active(value)
 
 	''' Update the window manager button layout '''
 	def applyWMChanges(self, widget): 
