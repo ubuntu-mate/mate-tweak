ba1cf969813572fe8cb75195896bcc25bde038ef
Ikey Doherty <contactjfreak@googlemail.com>
2010-04-17 20:21:27 +0100

    Begin rewrite

--- usr/lib/linuxmint/mintDesktop/mintDesktop.py
+++ usr/lib/linuxmint/mintDesktop/mintDesktop.py
@@ -17,15 +17,15 @@ except Exception, detail:
 
 
 # i18n
+# TODO: Badly need to fix this - overuse of "The" etc.
 gettext.install("mintdesktop", "/usr/share/linuxmint/locale")
 
 # i18n for menu item
-menuName = _("Desktop Settings")
-menuGenericName = _("Gnome Gonfiguration Tool")
+menuName = _("Desktop Configuration Tool")
+menuGenericName = _("Gnome Settings")
 menuComment = _("Fine-tune Gnome settings")
 
-class MintDesktop:	
-
+class MintDesktop:
 	"""MintDesktop - Makes the best out of your Gnome desktop..."""
 
 	# Set a string in gconf
@@ -38,70 +38,142 @@ class MintDesktop:
 		client = gconf.client_get_default()
 		return client.get_string(key)
 
-	# Set a boolean in gconf according to the value
+	# Set a boolean in gconf according to the value of the passed gtk.CheckButton
 	def set_bool(self, key, value):
 		client = gconf.client_get_default()
-		client.set_bool(key, value)
+		client.set_bool(key, value.get_active())
 
 	# Get a boolean from gconf
 	def get_bool(self, key):
 		client = gconf.client_get_default()
 		return client.get_bool(key)
 
+	# Change pages
+	def side_view_nav(self, param):
+		path = param.get_selected_items()
+		if (len(path) > 0):
+			selection = path[0][0]			
+			self.get_widget("notebook1").set_current_page(selection)
 
+	''' Create the UI '''
 	def __init__(self):
+		# load our glade ui file in
 		self.gladefile = '/usr/lib/linuxmint/mintDesktop/mintDesktop.glade'
 		self.wTree = gtk.glade.XML(self.gladefile, "main_window") 
+		self.get_widget("main_window").connect("destroy", gtk.main_quit)
 
-		self.wTree.get_widget("main_window").connect("destroy", gtk.main_quit)
-		self.wTree.get_widget("button_cancel").connect("clicked", gtk.main_quit)
-
-		# combobox
-		wmstyles = gtk.ListStore(str, str)
-		wmstyles.append([_("Traditional style"), "menu:minimize,maximize,close"])
-		wmstyles.append([_("Mac style"), "close,minimize,maximize:"])
-		wmstyles.append([_("Ubuntu style"), "maximize,minimize,close:"])
-		self.wTree.get_widget("combo_wmlayout").set_model(wmstyles)
-
-		# i18n
-		self.wTree.get_widget("main_window").set_title(_("Desktop Settings"))
-		self.wTree.get_widget("label3").set_text(_("Desktop Items"))
-		self.wTree.get_widget("label5").set_text(_("Window Manager"))
-		self.wTree.get_widget("label_layouts").set_text(_("Window buttons layout:"))
-		self.wTree.get_widget("checkbox_computer").set_label(_("Computer"))
-		self.wTree.get_widget("checkbox_home").set_label(_("Home"))
-		self.wTree.get_widget("checkbox_network").set_label(_("Network"))
-		self.wTree.get_widget("checkbox_trash").set_label(_("Trash"))
-		self.wTree.get_widget("checkbox_volumes").set_label(_("Mounted Volumes"))
-		self.wTree.get_widget("checkbox_compositing").set_label(_("Gnome compositing"))
-
-		# tell gconf we want to be notified when these change
+		# say hi to gconf
 		client = gconf.client_get_default()
 		client.add_dir("/apps/nautilus/desktop", gconf.CLIENT_PRELOAD_NONE)
 		client.add_dir("/apps/metacity/general", gconf.CLIENT_PRELOAD_NONE)
+		client.add_dir("/desktop/gnome/interface", gconf.CLIENT_PRELOAD_NONE)
+		client.add_dir("/apps/nautilus/preferences", gconf.CLIENT_PRELOAD_NONE)
 
-		# initialise the checkboxes		
+		# get the icon theme.
+		theme = gtk.icon_theme_get_default()
+		img = theme.load_icon("user-desktop", 36, 0)
+		# create the backing store for the side nav-view. very perty.
+		self.store = gtk.ListStore(str, gtk.gdk.Pixbuf)
+		self.store.append(["Desktop", img])
+		img = theme.load_icon("gnome-windows", 36, 0)
+		self.store.append(["Window Manager", img])
+		img = theme.load_icon("preferences-desktop", 36, 0)
+		self.store.append(["Interface", img])
+		img = theme.load_icon("preferences-desktop-wallpaper", 36, 0)
+
+		# set up the side view - navigation.
+      		self.get_widget("side_view").set_text_column(0)
+        	self.get_widget("side_view").set_pixbuf_column(1)
+		self.get_widget("side_view").set_model(self.store)
+		self.get_widget("side_view").select_path((0,))
+		self.get_widget("side_view").connect("selection_changed", self.side_view_nav )
+
+		# set up larger components.
+		self.get_widget("main_window").set_title("Desktop Configuration")
+		self.get_widget("main_window").connect("destroy", gtk.main_quit)
+		self.get_widget("button_cancel").connect("clicked", gtk.main_quit)
+
+		# i18n
+		self.get_widget("label3").set_text(_("Desktop Items"))
+		self.get_widget("label5").set_text("Window Manager Tweaks")
+		self.get_widget("checkbox_computer").set_label(_("Computer"))
+		self.get_widget("checkbox_home").set_label(_("Home"))
+		self.get_widget("checkbox_network").set_label(_("Network"))
+		self.get_widget("checkbox_trash").set_label(_("Trash"))
+		self.get_widget("checkbox_volumes").set_label(_("Mounted Volumes"))
+		self.get_widget("checkbox_compositing").set_label("Gnome compositing")		
+
+		# Desktop page
 		self.init_checkbox("/apps/nautilus/desktop/computer_icon_visible", "checkbox_computer")
 		self.init_checkbox("/apps/nautilus/desktop/home_icon_visible", "checkbox_home")
 		self.init_checkbox("/apps/nautilus/desktop/network_icon_visible", "checkbox_network")
 		self.init_checkbox("/apps/nautilus/desktop/trash_icon_visible", "checkbox_trash")
 		self.init_checkbox("/apps/nautilus/desktop/volumes_visible", "checkbox_volumes")
-		self.init_checkbox("/apps/metacity/general/compositing_manager", "checkbox_compositing")
 
-		# sets up the metacity button layout combobox
+		# Window Manager page
+		self.init_checkbox("/apps/metacity/general/reduced_resources", "checkbutton_resources")
+		self.init_checkbox("/apps/metacity/general/disable_workarounds", "checkbutton_workarounds")
+		self.init_checkbox("/apps/metacity/general/compositing_manager", "checkbox_compositing")
+		self.init_checkbox("/apps/metacity/general/titlebar_uses_system_font", "checkbutton_titlebar")
+
+		# interface page
+		self.init_checkbox("/desktop/gnome/interface/menus_have_icons", "checkbutton_menuicon")
+		self.init_checkbox("/desktop/gnome/interface/menus_have_tearoff", "checkbutton_menu_tearoff")
+		self.init_checkbox("/desktop/gnome/interface/menubar_detachable","checkbutton_menubarmove")
+		self.init_checkbox("/desktop/gnome/interface/can_change_accels", "checkbutton_acel_edit")
+		self.init_checkbox("/desktop/gnome/interface/toolbar_detachable", "checkbutton_toolbar_detach")
+		self.init_checkbox("/desktop/gnome/interface/show_input_method_menu","checkbutton_im_menu")
+		self.init_checkbox("/desktop/gnome/interface/show_unicode_menu", "checkbutton_unicode")
+		self.init_checkbox("/desktop/gnome/interface/buttons_have_icons", "checkbutton_button_icons")
+		self.init_checkbox("/desktop/gnome/interface/enable_animations", "checkbutton_animations")
+
+		iconSizes = gtk.ListStore(str, str)
+		iconSizes.append(["Small", "small-toolbar"])
+		iconSizes.append(["Large", "large-toolbar"])
+		self.get_widget("combobox_icon_size").set_model(iconSizes)
+		self.init_combobox("/desktop/gnome/interface/toolbar_icons_size", "combobox_icon_size")
+
+		# Metacity button layouts..
+		layouts = gtk.ListStore(str, str)
+		layouts.append(["Left", "minimize,maximize,close:menu"])
+		layouts.append(["Left (Inverted)", "close,minimize,maximize:menu"])
+		layouts.append(["Right", "menu:minimize,maximize,close"])
+		self.get_widget("combo_wmlayout").set_model(layouts)
 		self.init_combobox("/apps/metacity/general/button_layout", "combo_wmlayout")
+
+		# toolbar icon styles
+		iconStyles = gtk.ListStore(str, str)
+		iconStyles.append(["Text below items", "both"])
+		iconStyles.append(["Text beside items", "both-horiz"])
+		iconStyles.append(["Icons only", "icons"])
+		iconStyles.append(["Text only", "text"])
+		self.get_widget("combobox_toolicons").set_model(iconStyles)
+		self.init_combobox("/desktop/gnome/interface/toolbar_style", "combobox_toolicons")
+
+		# demo stuff (for the interface tab)
+		self.get_widget("toolbar1").add(gtk.ToolButton(gtk.STOCK_NEW))
+		self.get_widget("toolbar1").add(gtk.ToolButton(gtk.STOCK_OPEN))
+		self.get_widget("toolbar1").add(gtk.ToolButton(gtk.STOCK_SAVE))
+		self.get_widget("toolbar1").show_all()
+
+		self.get_widget("main_window").show()
+
+	''' Saves typing self.get_widget all the time.... '''
+	def get_widget(self, which):
+		return self.wTree.get_widget(which)
+
 
 	''' Initialise the CheckButton with a gconf value, then bind it with the gconf system '''
 	def init_checkbox(self, key, name):
-		widget = self.wTree.get_widget(name)
+		widget = self.get_widget(name)
 		conf = self.get_bool(key)
 		widget.set_active(conf)
-		widget.connect("clicked", lambda x: self.set_bool(key, x.get_active()))
+		widget.connect("clicked", lambda x: self.set_bool(key, x))
 		self.add_notify(key, widget)
 
 	''' Bind the ComboBox to gconf and assign the action '''
 	def init_combobox(self, key, name):
-		widget = self.wTree.get_widget(name)
+		widget = self.get_widget(name)
 		conf = self.get_string(key)
 		index = 0
 		for row in widget.get_model():
@@ -110,7 +182,6 @@ class MintDesktop:
 				break
 			index = index +1
 		widget.connect("changed", lambda x: self.combo_fallback(key, x))
-		self.add_notify(key, widget)
 	
 	''' Fallback for all combo boxes '''	
 	def combo_fallback(self, key, widget):
@@ -136,27 +207,27 @@ class MintDesktop:
 			client.notify_remove (notify_id)
 
 	''' Callback for gconf. update our internal values '''
-	def key_changed_callback (self, client, notify_id, entry, widget):
+	def key_changed_callback (self, client, cnxn_id, entry, widget):
 		# deal with all boolean (checkboxes)
 		if (type(widget) == gtk.CheckButton):
 			if(entry.value.type == gconf.VALUE_BOOL):
 				value = entry.value.get_bool()
 				if(widget):
 					widget.set_active(value)
+		# combobox, multiple targets..
 		elif( type(widget) == gtk.ComboBox ):
 			# Sanity check, if its crap ignore it.
 			if(entry.value.type == gconf.VALUE_STRING):
 				if(not widget and not value):
 					return
-				# the string in question :)
-				value = entry.value.get_string()
-				index = 0
-				for row in widget.get_model():
-					if(value == row[1]):
-						widget.set_active(index)
-						break
-					index = index +1
-			
+			# the string in question :)
+			value = entry.value.get_string()
+			for row in widget.get_model():
+				if(value == row[1]):
+					widget.set_active(index)
+					break
+				index = index +1
 if __name__ == "__main__":
 	MintDesktop()
 	gtk.main()
+
