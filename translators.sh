69e02bda9c5c7e7e3fcd9a6f1f4221d28925e357
Martin Wimpress <code@flexion.org>
2015-04-07 11:04:25 +0100

    Remove utility scripts.

--- translators.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Files: po/*" >  translators.txt
-echo "Copyright:"  >> translators.txt
-grep Last-Translator po/*.po | cut -d':' -f3 | sort -u | grep -v Unknown | grep -v "FULL NAME" | cut -d'\' -f1 >> translators.txt
