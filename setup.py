ed119ba353e368ecb7d9fac2958c97b4f47e4c0f
Martin Wimpress <code@flexion.org>
2015-05-18 19:18:12 +0100

    Add support for multiple window managers. Improved window manager switching. Fixed saving panel layouts. Minor code cleaning. Do not quit when switching window managers. Added notifications.

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ data_files = [
     ('{prefix}/share/applications'.format(prefix=sys.prefix), ['data/mate-tweak.desktop',]),
     ('{prefix}/share/mate-tweak'.format(prefix=sys.prefix), ['data/mate-volume-control-applet.desktop',]),
     ('{prefix}/share/polkit/actions'.format(prefix=sys.prefix), ['data/org.mate.mate-tweak.policy',]),
-    ('{prefix}/lib/mate-tweak'.format(prefix=sys.prefix), ['data/mate-tweak.ui', 'util/disable-mate-volume-applet', 'util/mate-panel-backup']),
+    ('{prefix}/lib/mate-tweak'.format(prefix=sys.prefix), ['data/mate-tweak.ui', 'util/disable-mate-volume-applet', 'util/mate-panel-backup', 'util/install-mate-panel-layout']),
 ]
 data_files.extend(datafilelist('{prefix}/share/locale'.format(prefix=sys.prefix), 'build/mo'))
 
