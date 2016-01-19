5015bc3df41335c1eba14ad0191883334d982b0e
Martin Wimpress <code@flexion.org>
2016-01-19 07:19:26 +0000

    Refactor to create no-composite wrappers for MATE and Metacity.

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ data_files = [
     ('{prefix}/share/applications'.format(prefix=sys.prefix), ['data/mate-tweak.desktop',]),
     ('{prefix}/share/mate/applications'.format(prefix=sys.prefix), ['data/marco-compton.desktop',]),
     ('{prefix}/share/mate/applications'.format(prefix=sys.prefix), ['data/metacity-compton.desktop',]),
+    ('{prefix}/share/mate/applications'.format(prefix=sys.prefix), ['data/marco-no-composite.desktop',]),
+    ('{prefix}/share/mate/applications'.format(prefix=sys.prefix), ['data/metacity-no-composite.desktop',]),
     ('{prefix}/share/mate-tweak'.format(prefix=sys.prefix), ['data/mate-volume-control-applet.desktop',]),
     ('{prefix}/share/polkit-1/actions'.format(prefix=sys.prefix), ['data/org.mate.mate-tweak.policy',]),
     ('{prefix}/lib/mate-tweak'.format(prefix=sys.prefix), ['data/mate-tweak.ui', 'util/disable-mate-volume-applet', 'util/mate-panel-backup', 'util/install-mate-panel-layout']),
@@ -80,6 +82,6 @@ setup(
     package_dir = {'': '.'},
     data_files = data_files,
     install_requires = [ 'setuptools', ],
-    scripts = ['mate-tweak', 'marco-compton', 'metacity-compton'],
+    scripts = ['mate-tweak', 'marco-compton', 'marco-no-composite', 'metacity-compton', 'metacity-no-composite'],
     cmdclass = cmdclass,
 )
